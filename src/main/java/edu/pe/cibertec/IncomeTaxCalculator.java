c5f3c577a8778796bdb422c4caac6dbfc7c1a536
Christiam Mendoza <ch.alb.rz@hotmail.com>
2026-03-20 21:04:19 -0500

    Income within bracket 12 to 27 UIT pay 8% on excess

--- src/main/java/edu/pe/cibertec/IncomeTaxCalculator.java
+++ src/main/java/edu/pe/cibertec/IncomeTaxCalculator.java
@@ -1,14 +1,14 @@
 package edu.pe.cibertec;
 
 public class IncomeTaxCalculator {
-    private  final double uit;
+    private final double uit;
 
-    public IncomeTaxCalculator(){
+    public IncomeTaxCalculator() {
         uit = 4_950.0;
     }
 
-    public IncomeTaxCalculator(double uit){
-        if (uit <= 0){
+    public IncomeTaxCalculator(double uit) {
+        if (uit <= 0) {
             throw new IllegalArgumentException("UIT must be a positive value");
         }
         this.uit = uit;
@@ -16,7 +16,7 @@ public class IncomeTaxCalculator {
 
 
     public double calculate(double annualIncome) {
-        if (annualIncome < 0 ){
+        if (annualIncome < 0) {
             throw new IllegalArgumentException("Annual income cannot be negative.");
         }
         double bracket1 = 7 * uit;
@@ -25,12 +25,23 @@ public class IncomeTaxCalculator {
         double bracket4 = 42 * uit;
         double bracket5 = 54 * uit;
 
-        if (annualIncome <= bracket1) return  0;
+        if (annualIncome <= bracket1) return 0;
         double tax = 0;
 
         tax += (Math.min(annualIncome, bracket2) - bracket1) * 0.08;
-        if(annualIncome<=bracket2) return tax;
+        if (annualIncome <= bracket2) return tax;
 
-        return  tax;
+        tax += (Math.min(annualIncome, bracket3) - bracket2) * 0.14;
+        if (annualIncome <= bracket3) return tax;
+
+        tax += (Math.min(annualIncome, bracket4) - bracket5) * 0.17;
+        if (annualIncome <= bracket4) return tax;
+
+        tax += (Math.min(annualIncome, bracket5) - bracket4) * 0.20;
+        if (annualIncome <= bracket5) return  tax;
+
+        tax += (annualIncome - bracket5) * 0.30;
+
+        return tax;
     }
 }
--- src/test/java/edu/pe/cibertec/IncomeTaxCalculatorTest.java
+++ src/test/java/edu/pe/cibertec/IncomeTaxCalculatorTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
+
 @DisplayName("IncomeTaxCalculator - Scaled Tax Brackets")
 public class IncomeTaxCalculatorTest {
 
@@ -13,29 +14,37 @@ public class IncomeTaxCalculatorTest {
     private static final double DELTA = 0.01;
 
     @BeforeEach
-    void setUp(){
+    void setUp() {
         calculator = new IncomeTaxCalculator();
     }
 
     @Test
     @DisplayName("Income below 7 UIT pays 0% tax")
-    void giverIncomeBelowSevenUIT_whenCalculate_thenZeroTax(){
+    void giverIncomeBelowSevenUIT_whenCalculate_thenZeroTax() {
         double income = 6 * UIT;
         assertEquals(0.0, calculator.calculate(income), DELTA);
     }
 
     @Test
     @DisplayName("Income exactly at 7 UIT pays 0% tax")
-    void giverIncomeExactlySevenUIT_whenCalculate_thenZeroTax(){
+    void giverIncomeExactlySevenUIT_whenCalculate_thenZeroTax() {
         double income = 7 * UIT;
         assertEquals(0.0, calculator.calculate(income), DELTA);
     }
 
     @Test
     @DisplayName("Income within bracket 7 to 12 UIT pay 8% on excess")
-    void giveIncomeInBracketSevenToTwelveUIT_whenCalcule_thenEightPercent(){
+    void giveIncomeInBracketSevenToTwelveUIT_whenCalculate_thenEightPercent() {
         double income = 10 * UIT;
-        double expected = (10 -7 )* UIT * 0.08;
+        double expected = (10 - 7) * UIT * 0.08;
+        assertEquals(expected, calculator.calculate(income), DELTA);
+    }
+
+    @Test
+    @DisplayName("Income within bracket 12 to 27 UIT pay 8% on excess")
+    void giveIncomeInBracketTwelveToTwentySevenUIT_whenCalculate_thenAccumulateTax() {
+        double income = 20 * UIT;
+        double expected = (5 * UIT * 0.08) + (8 * UIT * 0.14);
         assertEquals(expected, calculator.calculate(income), DELTA);
     }
 
