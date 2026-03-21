e6d0872010c8a3ce05b9ca20b398de04b75f06ba
Christiam Mendoza <ch.alb.rz@hotmail.com>
2026-03-20 20:03:06 -0500

    feat(basic): triangle calssifier with TDD

--- src/main/java/edu/pe/cibertec/Triangle.java
+++ src/main/java/edu/pe/cibertec/Triangle.java
@@ -11,13 +11,21 @@ public class Triangle {
     }
 
     public TriangleType classfy(int a, int b, int c) {
+        if (!isValid(a, b, c)) {
+            return TriangleType.INVALID;
+        }
         if (a == b && b == c) {
             return TriangleType.EQUILATERAL;
         }
-        if (a == b || b == c || a == c  ){
-            return  TriangleType.ISOSCELES;
+        if (a == b || b == c || a == c) {
+            return TriangleType.ISOSCELES;
         }
         return TriangleType.SCALENE;
+    }
+
+    private boolean isValid(int a, int b, int c) {
+        if (a <= 0 || b <= 0 || c <= 0) return false;
+        return (a < b + c) && (b < a + c) && (c < a + b);
     }
 
 }
--- src/test/java/edu/pe/cibertec/TriangleTest.java
+++ src/test/java/edu/pe/cibertec/TriangleTest.java
@@ -4,8 +4,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static edu.pe.cibertec.Triangle.TriangleType.EQUILATERAL;
-import static edu.pe.cibertec.Triangle.TriangleType.ISOSCELES;
+import static edu.pe.cibertec.Triangle.TriangleType.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 @DisplayName("Triangle - Triangle Classifier")
@@ -25,8 +24,38 @@ public class TriangleTest {
 
     @Test
     @DisplayName("Two equal sides returns ISOSCELES")
-    void givenTwoEqualSides_whenClassfiy_thenIsosceles(){
+    void givenTwoEqualSides_whenClassify_thenIsosceles(){
         assertEquals(ISOSCELES, triangle.classfy(5,5,4));
+    }
+
+    @Test
+    @DisplayName("Two equal sides returns SCALENE")
+    void givenTwoEqualSides_whenClassify_thenScalene(){
+        assertEquals(SCALENE, triangle.classfy(6,5,4));
+    }
+
+    @Test
+    @DisplayName("Side equal to sum of other two returns INVALID")
+     void givenSideEqualToSumOfOtherTwo_whenClassify_thenInvalid(){
+        assertEquals(INVALID, triangle.classfy(1,2,3));
+    }
+
+    @Test
+    @DisplayName("Side greater than sum of other two returns INVALID")
+    void givenSideEqualToGraterThanSumOfOtherTwo_whenClassify_thenInvalid(){
+        assertEquals(INVALID, triangle.classfy(1,2,10));
+    }
+
+    @Test
+    @DisplayName("Side with zero value returns INVALID")
+    void givenZeroSide_whenClassify_thenInvalid(){
+        assertEquals(INVALID, triangle.classfy(8,0,8));
+    }
+
+    @Test
+    @DisplayName("Side with negative value returns INVALID")
+    void givenNegativeSide_whenClassify_thenInvalid(){
+        assertEquals(INVALID, triangle.classfy(8,-4,8));
     }
 
 }
